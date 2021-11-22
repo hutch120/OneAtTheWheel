82f3bfc94f4d75b776069d43e49c9b916fb433d6
Simon <hutch120@gmail.com>
2021-11-23 10:41:17 +1100

    Add bucket for data

--- server/course/CourseService.js
+++ server/course/CourseService.js
@@ -1,6 +1,13 @@
+const s3 = require('../awsutils/s3')
+
 async function getCourse ({ name }) {
   if (!name || name === '') return { success: false }
-  return { success: true, data: name }
+  const response = s3.create({ filename: 'test123.json', data: { test: 'abc' } })
+  if (response.success) {
+    return { success: true, data: name }
+  } else {
+    return { success: false, message: 'Failed to create file on AWS S3.' }
+  }
 }
 
 module.exports = {
--- server/routes/Routes.test.js
+++ server/routes/Routes.test.js
@@ -1,11 +1,20 @@
 /* eslint-env jest */
+const AWS = require('aws-sdk')
 const request = require('supertest')
+const secrets = require('../secrets.js')
 
 describe('Routes', () => {
   const Routes = require('./Routes')
   const server = Routes.Initialize()
 
-  // beforeAll(async () => { })
+  beforeAll(async () => {
+    const accessKeyId = secrets?.AWS_ACCESS_KEY_ID ?? ''
+    const secretAccessKey = secrets?.AWS_SECRET_ACCESS_KEY ?? ''
+    const region = 'ap-southeast-2'
+    const awsConfig = { accessKeyId, secretAccessKey, region }
+    // console.log('awsConfig', awsConfig)
+    AWS.config.update(awsConfig)
+  })
 
   describe('Routes General', () => {
     it('should return 404', async () => {
